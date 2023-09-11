3a1f6ef1eec5345f415a232170a3f15c19cffee3
Matt Russell <m.russell@bangor.ac.uk>
2023-09-11 11:32:31 +0100

    Clean up, remove redundant code.

--- spacy/lang/cy/lemmatizer.py
+++ spacy/lang/cy/lemmatizer.py
@@ -1,12 +1,10 @@
-from typing import List, Dict, Tuple
+from typing import List, Tuple
 
 from ...pipeline import Lemmatizer
 from ...tokens import Token
 
 
 class WelshLemmatizer(Lemmatizer):
-    """XXXX"""
-
     @classmethod
     def get_lookups_config(cls, mode: str) -> Tuple[List[str], List[str]]:
         if mode == "lookup":
@@ -34,55 +32,14 @@ class WelshLemmatizer(Lemmatizer):
         lookup_pos = univ_pos.lower()
         lookup_table = self.lookups.get_table("lemma_lookup_" + lookup_pos, {})
         if univ_pos == "NOUN":
-            return self.lemmatize_noun(string, univ_pos, lookup_table)
+            lemma = self.lemmatize_noun(string, univ_pos, lookup_table)
         else:
             if univ_pos != "PROPN":
                 string = string.lower()
-            if univ_pos == "DET":
-                return self.lemmatize_det(string, univ_pos, lookup_table)
-            elif univ_pos == "PRON":
-                return self.lemmatize_pron(string, univ_pos, lookup_table)
-            elif univ_pos == "ADP":
-                return self.lemmatize_adp(string, univ_pos, lookup_table)
-            elif univ_pos == "ADJ":
-                return self.lemmatize_adj(string, univ_pos, lookup_table)
+            if univ_pos in {"DET", "PRON", "ADP", "ADJ"}:
+                lemma = lookup_table.get(string, string)
             else:
                 lemma = lookup_table.get(string, "")
-        # if not lemma:
-        #    lookup_table = self.lookups.get_table("lemma_lookup_other")
-        #    lemma = lookup_table.get(string, "")
-        # if not lemma:
-        #    lookup_table = self.lookups.get_table(
-        #        "lemma_lookup"
-        #    )  # "legacy" lookup table
-        #    lemma = lookup_table.get(string, string.lower())
         if not lemma:
             lemma = token.text
         return [lemma]
-
-    def lemmatize_det(
-        self, string: str, univ_pos: str, lookup_table: Dict[str, str]
-    ) -> List[str]:
-        return [lookup_table.get(string, string)]
-
-    def lemmatize_pron(
-        self, string: str, univ_pos: str, lookup_table: Dict[str, str]
-    ) -> List[str]:
-        lemma = lookup_table.get(string, string)
-        return [lemma]
-
-    def lemmatize_adp(
-        self, string: str, univ_pos: str, lookup_table: Dict[str, str]
-    ) -> List[str]:
-        return [lookup_table.get(string, string)]
-
-    def lemmatize_adj(
-        self, string: str, univ_pos: str, lookup_table: Dict[str, str]
-    ) -> List[str]:
-        lemma = lookup_table.get(string, string)
-        return [lemma]
-
-    def lemmatize_noun(
-        self, string: str, univ_pos: str, lookup_table: Dict[str, str]
-    ) -> List[str]:
-        return [lookup_table.get(string, string)]
